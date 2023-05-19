b1cf5dbdb10f6f5b4d13ab6f05fae27ce3b9032f
theteamisbest <abhithory@gmail.com>
2023-05-20 00:39:48 +0530

    child contract created

--- contracts/ChildVaccination.sol
+++ contracts/ChildVaccination.sol
@@ -3,8 +3,14 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract ChildContract {
-    address private childAddr;
-    address private registrarAddr;
+    bytes32 private constant ON_BIRTH = keccak256(abi.encodePacked("birth"));
+    bytes32 private constant ON_2M_15D = keccak256(abi.encodePacked("2m15d"));
+    bytes32 private constant ON_3M_15D = keccak256(abi.encodePacked("3m15d"));
+    bytes32 private constant ON_9M = keccak256(abi.encodePacked("9m"));
+    bytes32 private constant ON_16M = keccak256(abi.encodePacked("16m"));
+    bytes32 private constant ON_5Y = keccak256(abi.encodePacked("5y"));
+    bytes32 private constant ON_10Y = keccak256(abi.encodePacked("10y"));
+    bytes32 private constant ON_16Y = keccak256(abi.encodePacked("16y"));
 
     struct OneVaccination {
         string vaccName;
@@ -12,93 +18,60 @@ contract ChildContract {
         uint256 vaccDate;
     }
 
-    struct BirthStruct {
-        OneVaccination OPV0;
-        OneVaccination HepB;
-        OneVaccination BCG;
-    }
+    mapping(string => mapping(string => OneVaccination)) private childDetails;
 
-    struct M1D15Struct {
-        OneVaccination OPV1;
-        OneVaccination PENTA1;
-        OneVaccination ROTA1;
-        OneVaccination PCV1;
-        OneVaccination IPV1;
-    }
-
-    struct M2D15Struct {
-        OneVaccination OPV2;
-        OneVaccination PENTA2;
-        OneVaccination ROTA2;
-    }
-
-    struct M3D15Struct {
-        OneVaccination OPV3;
-        OneVaccination PENTA3;
-        OneVaccination ROTA3;
-        OneVaccination PCV2;
-        OneVaccination IPV2;
-    }
-
-    struct M9Struct {
-        OneVaccination MR1;
-        OneVaccination JE1;
-        OneVaccination VA1;
-        OneVaccination PCV3;
-    }
-
-    struct M16Struct {
-        OneVaccination DPTB1;
-        OneVaccination VA2;
-        OneVaccination MR2;
-        OneVaccination JE2;
-        OneVaccination OPVB;
-    }
-
-    struct Y5Struct {
-        OneVaccination DPTB2;
-    }
-
-    struct Y10Struct {
-        OneVaccination TT;
-    }
-
-    struct Y16Struct {
-        OneVaccination TT;
-    }
-
-    struct ChildVaccinationStruct {
-        BirthStruct onBirth;
-        M1D15Struct on1M15DBirth;
-        M2D15Struct on2M15DBirth;
-        M3D15Struct on3M15DBirth;
-        M9Struct on9MBirth;
-        M16Struct on16m24Birth;
-        Y5Struct on5y6Birth;
-        Y10Struct on10YBirth;
-        Y16Struct on16YBirth;
-    }
-
-    ChildVaccinationStruct childDetails;
+    address private childAddr;
+    address private registrarAddr;
 
     constructor(address _childAddr) {
         childAddr = _childAddr;
         registrarAddr = msg.sender;
     }
 
-    function getChildDetails()
-        public
-        view
-        returns (ChildVaccinationStruct memory)
-    {
-        return childDetails;
+    modifier onlyChildUser() {
+        require(msg.sender == childAddr);
+        _;
+    }
+    modifier onlyRegistrarUser() {
+        require(msg.sender == registrarAddr);
+        _;
     }
 
-    function setChildDetails() public {
-        childDetails.onBirth = BirthStruct(
-            OneVaccination("vacc11", true, block.timestamp),
-            OneVaccination("vacc11", true, block.timestamp),
-            OneVaccination("vacc11", true, block.timestamp)
+    modifier childOrRegistrarBoth() {
+        require(msg.sender == childAddr || msg.sender == registrarAddr);
+        _;
+    }
+
+    modifier AgeTypeShouldBeInFormat(string memory childAgeType) {
+        bytes32 childAgeTypeBytes = keccak256(abi.encodePacked(childAgeType));
+        require(
+            ON_BIRTH == childAgeTypeBytes ||
+                ON_2M_15D == childAgeTypeBytes ||
+                ON_3M_15D == childAgeTypeBytes ||
+                ON_9M == childAgeTypeBytes ||
+                ON_16M == childAgeTypeBytes ||
+                ON_5Y == childAgeTypeBytes ||
+                ON_10Y == childAgeTypeBytes ||
+                ON_16Y == childAgeTypeBytes
+        );
+        _;
+    }
+
+    function getChildDetails(
+        string memory childAgeType,
+        string memory vaccineName
+    ) public view childOrRegistrarBoth returns (OneVaccination memory) {
+        return childDetails[childAgeType][vaccineName];
+    }
+
+    function updateVaccinationStatusBirth(
+        string memory childAgeType,
+        string memory vaccineName
+    ) public onlyRegistrarUser AgeTypeShouldBeInFormat(childAgeType) {
+        childDetails[childAgeType][vaccineName] = OneVaccination(
+            vaccineName,
+            true,
+            block.timestamp
         );
     }
 }
